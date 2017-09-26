0048b2f8e8c8e42e23469924cb736c83d9c8b22e
Dave Harms <daveharmswebdev@gmail.com>
2017-09-26 11:00:20 -0500

    added angular material and flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MdButtonModule,
+  MatMenuModule
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
