118fa2d2ad1b1bb73f71c21d823e0a2572271d91
Dave <daveharmswebdev@gmail.com>
2017-10-06 16:25:08 -0400

    added environment back

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCpB6IDgQExBy1gsSr5rmCbB9ZpCbYMxgw',
+    authDomain: 'c247-ehr.firebaseapp.com',
+    databaseURL: 'https://c247-ehr.firebaseio.com',
+    projectId: 'c247-ehr',
+    storageBucket: 'c247-ehr.appspot.com',
+    messagingSenderId: '681075832740'
+  }
+};
